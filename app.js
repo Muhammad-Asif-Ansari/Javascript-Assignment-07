6d4f8c78fc23cd7584bde348e903b67125d0dc57
Muhammad-Asif-Ansari <muhammadasifansari101@gmail.com>
2024-10-03 19:56:53 +0500

    JavaScript Assignment
    JavaScript Assignment

---
+++ app.js
@@ -0,0 +1,11 @@
+// Assignment 7:
+
+// var famousWord = prompt("Enter a Word");
+// var reverseWord = famousWord.split("").reverse("").join("");
+
+// if(famousWord === reverseWord){
+//     console.log("This word is a Palindrome");
+// }
+// else{
+//     console.log("This word is not a Palindrome");
+// }
